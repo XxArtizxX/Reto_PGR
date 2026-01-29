7e3ce595f89606de5888d1b76618b76347998cce
idont <idont@Pc_Artiz>
2026-01-29 23:37:22 +0100

    clase cliente finiquitada

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,5 +1,43 @@
 package clases;
 
-public class Cliente extends Persona{
+public class Cliente extends Persona {
 
+	private int codigoCliente;
+	private int totalCompras;
+
+	public Cliente(String dni, String nombre, String tipoPersona, int codigoCliente) {
+		super(dni, nombre, tipoPersona);
+		this.codigoCliente = codigoCliente;
+		this.totalCompras = 0;
+	}
+
+	public Cliente() {
+		super();
+	}
+
+	public int getCodigoCliente() {
+		return codigoCliente;
+	}
+
+	public void setCodigoCliente(int codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+
+	public int getTotalCompras() {
+		return totalCompras;
+	}
+
+	public void setTotalCompras(int totalCompras) {
+		this.totalCompras = totalCompras;
+	}
+
+	public void realizarCompra() {
+		this.totalCompras++;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [codigoCliente=" + codigoCliente + ", totalCompras=" + 
+				totalCompras + ", dni=" + dni + ", nombre=" + nombre +  ", tipoPersona=" + tipoPersona + "]";
+	}
 }
