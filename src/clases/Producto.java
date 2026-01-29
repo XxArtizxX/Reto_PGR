6922113e76940a34111e79d1a6cb1e6de41e51e0
1dami <1dami@HZ395670.TartangaLH.eus>
2026-01-29 14:21:59 +0100

    he cmabiado la clas eproducto

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -1,5 +1,11 @@
 package clases;
 
-public class Producto {
+import java.util.ArrayList;
+import enumeraciones.TipoCarta;
 
+public class Producto {
+	private TipoCarta tipoCaja;
+	private double precio;
+	private int stock;
+	private ArrayList<Integer> Cantidad;		
 }
