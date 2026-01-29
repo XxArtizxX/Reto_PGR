99eeb1aee4a79763f0353691558e5df4349a04a7
hodeipolo308@gmail.com <hodeipolo308@gmail.com>
2026-01-29 13:52:40 +0100

    Subo la clase Persona

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,5 +1,42 @@
 package clases;
 
-public class Persona {
+public  class Persona {
+	protected String dni;
+	protected String nombre;
+	protected String tipoPersona;
+	
+	public Persona(String dni, String nombre, String tipoPersona) {
+		this.dni=dni;
+		this.nombre=nombre;
+		this.tipoPersona=tipoPersona;
+	}
 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipoPersona() {
+		return tipoPersona;
+	}
+
+	public void setTipoPersona(String tipoPersona) {
+		this.tipoPersona = tipoPersona;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", tipoPersona=" + tipoPersona + "]";
+	}
 }
