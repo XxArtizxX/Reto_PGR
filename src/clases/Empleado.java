b770b4016b1cb665cf8ba4af1ef99fae62b372a2
hodeipolo308@gmail.com <hodeipolo308@gmail.com>
2026-01-30 10:52:36 +0100

    subo empleado

--- src/clases/Empleado.java
+++ src/clases/Empleado.java
@@ -1,11 +1,30 @@
 package clases;
-
 public class Empleado extends Persona{
-
 	private int cod_Cliente;
 	private int cantidad_Compras;
-	
 	public Empleado() {
 		super();
+	}
+	public Empleado(String dni, String nombre, String tipoPersona, int cod_Cliente, int cantidad_Compras) {
+		super(dni, nombre, tipoPersona);
+		this.cod_Cliente=cod_Cliente;
+		this.cantidad_Compras=cantidad_Compras;
+	}
+	public int getCod_Cliente() {
+		return cod_Cliente;
+	}
+	public void setCod_Cliente(int cod_Cliente) {
+		this.cod_Cliente = cod_Cliente;
+	}
+	public int getCantidad_Compras() {
+		return cantidad_Compras;
+	}
+	public void setCantidad_Compras(int cantidad_Compras) {
+		this.cantidad_Compras = cantidad_Compras;
+	}
+	@Override
+	public String toString() {
+		return "Empleado [cod_Cliente=" + cod_Cliente + ", cantidad_Compras=" + cantidad_Compras + ", dni=" + dni
+				+ ", nombre=" + nombre + ", tipoPersona=" + tipoPersona + "]";
 	}
 }
