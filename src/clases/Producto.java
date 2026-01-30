c5b8ece3b21085f7c1f4b02ca1707a93f052e77b
1dami <1dami@HZ395670.TartangaLH.eus>
2026-01-30 11:34:54 +0100

    he aninanido la clase producto

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -7,5 +7,43 @@ public class Producto {
 	private TipoCarta tipoCaja;
 	private double precio;
 	private int stock;
-	private ArrayList<Integer> Cantidad;		
+	private ArrayList<Integer> Cantidad;
+
+	public Producto(TipoCarta tipoCaja, double precio, int stock, ArrayList<Integer> cantidad) {
+		this.tipoCaja = tipoCaja;
+		this.precio = precio;
+		this.stock = 200;
+		Cantidad = cantidad;
+	}
+	public TipoCarta getTipoCaja() {
+		return tipoCaja;
+	}
+	public void setTipoCaja(TipoCarta tipoCaja) {
+		this.tipoCaja = tipoCaja;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public ArrayList<Integer> getCantidad() {
+		return Cantidad;
+	}
+	public void setCantidad(ArrayList<Integer> cantidad) {
+		Cantidad = cantidad;
+	}
+	@Override
+	public String toString() {
+		return "Producto [tipoCaja=" + tipoCaja + ", precio=" + precio + ", stock=" + stock + ", Cantidad=" + Cantidad
+				+ "]";
+	}	
 }
+
+
